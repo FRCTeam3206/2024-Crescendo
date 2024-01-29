a6a13d7c80b807ca1ef2f82dd543cd4d2d0a401e
CorrieMattison <147779386+CorrieMattison@users.noreply.github.com>
2024-01-29 13:03:52 -0600

    Added reset odometry button

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -15,6 +15,7 @@ import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -76,6 +77,7 @@ public class RobotContainer implements Logged {
     m_driverController.button(2).whileTrue(m_robotDrive.setXCommand());
     m_driverController.button(4).whileTrue(new TrapezoidalDriveToPosition(new Pose2d(1.8,.5,new Rotation2d(-Math.PI/2)), .05, .05, m_robotDrive));
     SmartDashboard.putData("Reset Gyro", m_robotDrive.zeroHeadingCommand());
+    SmartDashboard.putData("Reset Odometry", new InstantCommand(() -> m_robotDrive.resetOdometry(m_robotDrive.getPose())));
   }
 
   /**
