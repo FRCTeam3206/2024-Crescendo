9a2df00e59a5b403a1f6819083d6a86b927eeda4
CorrieMattison <147779386+CorrieMattison@users.noreply.github.com>
2024-08-15 22:07:03 -0500

    Basic controls with bumpers, backup with buttons.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -139,11 +139,12 @@ public class RobotContainer implements Logged {
     // xbox.povRight().onTrue(arm.ampPosition());
     // xbox.povLeft().onTrue(arm.subwooferPosition());
     // xbox.a().whileTrue(shootake.intakeCommand());
-    xbox.a().whileTrue(shootake.fastSlowIntakeCommand(() -> arm.getAngle() > 2.0));
+    xbox.leftBumper().whileTrue(shootake.fastSlowIntakeCommand(() -> arm.getAngle() > 2.0));
+    xbox.rightBumper().whileTrue(shootake.speakerShootCommand());
     xbox.b().onTrue(shootake.speakerShootCommand());
-    // xbox.y().whileTrue(shootake.ampCommand());
+    xbox.y().whileTrue(shootake.ampCommand());
     xbox.y().whileTrue(shootake.outakeCommand());
-    // xbox.x().whileTrue(shootake.slowIntakeCommand());
+    xbox.x().whileTrue(shootake.slowIntakeCommand());
 
     SmartDashboard.putData("Reset Gyro", m_robotDrive.zeroHeadingCommand());
     SmartDashboard.putData("Reset Climber", climber.zero());
