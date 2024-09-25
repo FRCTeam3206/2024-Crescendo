71de2d6189fe3b2b3e96fca028751452b2519ad3
CREATE-02 <frc-create@outlook.com>
2024-09-25 08:51:40 -0500

    Tune arm shoot position.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -226,7 +226,7 @@ public final class Constants {
     public static final double kArmZeroThreshold = .15;
 
     public static final double kArmAmpAngle = 2.0;
-    public static final double kShootAngle = 0.069;
+    public static final double kShootAngle = -0.20; // 0.069;
     public static final double kIntakeAngle = 3.476;
     public static final double kSubwooferAngle = 1.51;
 
