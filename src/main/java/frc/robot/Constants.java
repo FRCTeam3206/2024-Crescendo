895c74cde824279a68ec8ed224f624c1c165b0e8
CREATE-01 <frc-create@outlook.com>
2024-09-24 19:23:54 -0500

    Tune retained value.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -150,8 +150,8 @@ public final class Constants {
     public static final double kShootakeFreeSpeed = 5100.0;
     public static final double kShootakeLoadSpeedThreshold = 4750.0;
 
-    public static final double kRetainedValue = 0;
-    public static final double kNotRetainedValue = 1;
+    public static final double kRetainedValue = 0.8;
+    public static final double kNotRetainedValue = 0.35;
   }
 
   public static final class ArmConstants {
