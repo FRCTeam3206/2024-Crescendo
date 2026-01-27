5b19cded5d6542a6f2511313deb95d7bc4fb2e4d
CorrieMattison <147779386+CorrieMattison@users.noreply.github.com>
2026-01-27 11:48:22 -0600

    Adjusted shooting logic because the bottom wheel isn't working.

--- src/main/java/frc/robot/subsystems/Shootake.java
+++ src/main/java/frc/robot/subsystems/Shootake.java
@@ -31,7 +31,7 @@ public class Shootake extends SubsystemBase implements Logged {
 
   @Log
   private double getAverageSpeed() {
-    return -(topRoller.getEncoder().getVelocity() + bottomRoller.getEncoder().getVelocity()) / 2;
+    return bottomRoller.getEncoder().getVelocity();
   }
 
   @Log
