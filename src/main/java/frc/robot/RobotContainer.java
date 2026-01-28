a852c365d086fa08b844e9afe8b613d549f70a5e
CorrieMattison <147779386+CorrieMattison@users.noreply.github.com>
2026-01-27 18:09:38 -0600

    Fixed driving mode.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -175,7 +175,7 @@ public class RobotContainer implements Logged {
         new ParallelRaceGroup(arm.intakeCommandStop(), shootake.intakeCommand()),
         new ParallelCommandGroup(
                 m_robotDrive.driveCommand(
-                    () -> 0.15, () -> 0.0, () -> 0.0, () -> RelativeTo.ROBOT_RELATIVE, true),
+                    () -> 0.15, () -> 0.0, () -> 0.0, () -> RelativeTo.FIELD_RELATIVE, true),
                 arm.intakePosition(),
                 shootake.intakeCommand())
             .until(() -> shootake.hasNote())
